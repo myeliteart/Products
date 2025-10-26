50babba6b65f265b3a93a7acf7d6854474e6468a
myeliteart <eliteny0@gmail.com>
2025-10-26 05:52:36 -0400

    update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,10 +4,7 @@
     <h2>About us</h2>
     <div class="col-lg-6 about">
       <p class="py-1">
-         Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
-      </p>
-      <p>
-         Sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
+        Welcome to vue by eli, your destination for discovering and collecting your favorite music albums. Were passionate about bringing together music lovers and timeless collections, offering an easy and enjoyable browsing experience. Our goal is to make finding, comparing, and purchasing albums simple — with clean design, fast navigation, and smart features built using modern web technologies.
       </p>
     </div>
     <div class="col-lg-6">
